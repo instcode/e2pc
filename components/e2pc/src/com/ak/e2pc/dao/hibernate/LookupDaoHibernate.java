6436aceb5376b6cb1e0c4c0278cc176960630c15
tieuphongit <tieuphongit@f0cce744-c1bb-11dd-8bad-cde060d8fd3c>
2008-12-07 16:01:39 +0000

    initialization

---
+++ components/e2pc/src/com/ak/e2pc/dao/hibernate/LookupDaoHibernate.java
@@ -0,0 +1,24 @@
+package com.ak.e2pc.dao.hibernate;
+
+import java.util.List;
+
+import com.ak.e2pc.dao.LookupDao;
+import com.ak.e2pc.model.Role;
+
+/**
+ * Hibernate implementation of LookupDao.
+ *
+ * @author <a href="mailto:matt@raibledesigns.com">Matt Raible</a>
+ */
+public class LookupDaoHibernate extends UniversalDaoHibernate implements LookupDao {
+
+    /**
+     * {@inheritDoc}
+     */
+    @SuppressWarnings("unchecked")
+    public List<Role> getRoles() {
+        log.debug("Retrieving all role names...");
+
+        return getHibernateTemplate().find("from Role order by name");
+    }
+}
---
+++ components/e2pc/src/com/ak/e2pc/dao/hibernate/RoleDaoHibernate.java
@@ -0,0 +1,43 @@
+package com.ak.e2pc.dao.hibernate;
+
+import java.util.List;
+
+import com.ak.e2pc.dao.RoleDao;
+import com.ak.e2pc.model.Role;
+
+
+/**
+ * This class interacts with Spring's HibernateTemplate to save/delete and
+ * retrieve Role objects.
+ *
+ * @author <a href="mailto:bwnoll@gmail.com">Bryan Noll</a> 
+ */
+public class RoleDaoHibernate extends GenericDaoHibernate<Role, Long> implements RoleDao {
+
+    /**
+     * Constructor to create a Generics-based version using Role as the entity
+     */
+    public RoleDaoHibernate() {
+        super(Role.class);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Role getRoleByName(String rolename) {
+        List roles = getHibernateTemplate().find("from Role where name=?", rolename);
+        if (roles.isEmpty()) {
+            return null;
+        } else {
+            return (Role) roles.get(0);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void removeRole(String rolename) {
+        Object role = getRoleByName(rolename);
+        getHibernateTemplate().delete(role);
+    }
+}
