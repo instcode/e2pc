58c33ae123f8cd9516f1cd6e78fdf86d70769a22
instcode <instcode@f0cce744-c1bb-11dd-8bad-cde060d8fd3c>
2008-12-14 19:09:12 +0000

    Add user/admin services

--- trunk/components/e2pc/src/com/ak/e2pc/service/LoginService.java
+++ trunk/components/e2pc/src/com/ak/e2pc/service/LoginService.java
@@ -1,11 +1,13 @@
 package com.ak.e2pc.service;
 
-import org.springframework.security.annotation.Secured;
 
 public class LoginService {
 
-   @Secured({"ROLE_USER"})
-   public String getUserData() {
-      return "";        
-   }
+	public String getUserData() {
+		return "";
+	}
+
+	public String getAdminData() {
+		return "";
+	}
 }
